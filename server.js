9e18a4c84b5afe025631536cffd32ce9917186b7
elliot <elliotching@hotmail.com>
2024-07-17 20:45:33 +0800

    notes:
    - add mongodb and /comment to query 10 comments and format json returned

--- server.js
+++ server.js
@@ -1,98 +1,109 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
-// create express app
-const app = express();
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// Configuring the database
-// const dbConfig = require("./config/database.config.js");
 const mongoose = require("mongoose");
 const isDocker = require("is-docker");
-
-mongoose.Promise = global.Promise;
-
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 3000;
 
-function mongoUrl(port) {
-    if (isDocker()) {
-        return (
-            "mongodb://mongodd:" +
-            port +
-            "/db_nodejs_user_cars"
-        );
+async function main() {
+    // create express app
+    const app = express();
+
+    // parse requests of content-type - application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    // parse requests of content-type - application/json
+    app.use(bodyParser.json());
+
+    const uri =
+        "mongodb+srv://elliotching:BrZGKaBdWvtc2KEY@iad72njd.cr5gg5j.mongodb.net/";
+    const client = new MongoClient(uri);
+    // const client = new MongoClient(uri, {
+    //     auth: {
+    //         username: "elliotching",
+    //         password: "BrZGKaBdWvtc2KEY",
+    //     },
+    // });
+
+    try {
+        await client.connect();
+    } finally {
+        await client.close();
     }
+    // Connecting to the database
+    // mongoose
+    //     .connect(mongoUrl(), {
+    //         auth: {
+    //             user: "elliotching",
+    //             password: "BrZGKaBdWvtc2KEY",
+    //         },
+    //         useNewUrlParser: true,
+    //     })
+    //     .then(() => {
+    //         console.log(
+    //             "Successfully connected to the database\n" +
+    //                 mongoUrl()
+    //         );
+    //     })
+    //     .catch((err) => {
+    //         console.log(
+    //             "Could not connect to the database. Exiting now...\n",
+    //             err
+    //         );
+    //         process.exit();
+    //     });
+    // define a simple route
+    app.get("/", (req, res) => {
+        res.json({
+            message:
+                "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.",
+        });
+    });
+    app.get("/hello", (req, res) => {
+        res.json({
+            message: "Hello World",
+        });
+    });
+    app.get("/comment", async (req, res) => {
+        try {
+            await client.connect();
+            const database = client.db("sample_mflix");
+            const collection =
+                database.collection("comments");
+            let comments = await collection
+                .find({})
+                .limit(10)
+                .toArray();
+            // console.log(JSON.stringify(comment));
 
-    return (
-        "mongodb://localhost:" +
-        port +
-        "/db_nodejs_user_cars"
-    );
+            comments.map((comment) => {
+                return {
+                    name: comment.name,
+                    email: comment.email,
+                    movie_id: comment.movie_id,
+                    text: comment.text,
+                    date: comment.date,
+                };
+            });
+            // res.json(comment);
+
+            res.json({
+                data: comments,
+            });
+            // res.json(JSON.stringify(comments));
+        } finally {
+            await client.close();
+        }
+    });
+
+    require("./app/routes/routes.js")(app);
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
 }
-// Connecting to the database
-console.log("Connecting to the database\n");
-console.log("Connecting to the database\n");
-// mongoose
-//   .connect(mongoUrl("27017"), {
-//     useNewUrlParser: true,
-//   })
-//   .then(() => {
-//     console.log("Successfully connected to the database\n" + mongoUrl("27017"));
-//   })
-//   .catch((err) => {
-//     console.log("Could not connect to the database. Exiting now...\n", err);
-//     process.exit();
-//   });
 
-// mongoose
-// .connect(mongoUrl("28017"), {
-//   useNewUrlParser: true,
-// })
-// .then(() => {
-//   console.log("Successfully connected to the database\n" + mongoUrl("28017"));
-// })
-// .catch((err) => {
-//   console.log("Could not connect to the database. Exiting now...\n", err);
-//   console.log("Retry in port 27017...");
-//   mongoose
-//     .connect(mongoUrl("27017"), {
-//       useNewUrlParser: true,
-//     })
-//     .then(() => {
-//       console.log(
-//         "Successfully connected to the database\n" + mongoUrl("27017")
-//       );
-//     })
-//     .catch((err) => {
-//       console.log("Could not connect to the database. Exiting now...\n", err);
-//       process.exit();
-//     });
-// });
-
-// define a simple route
-app.get("/", (req, res) => {
-    res.json({
-        message:
-            "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes.",
-    });
-});
-app.get("/hello", (req, res) => {
-    res.json({
-        message: "Hello World",
-    });
-});
-
-require("./app/routes/routes.js")(app);
-
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
-
+main();
 // my atlas online mongodb::
 // mongosh "mongodb+srv://iad72njd.cr5gg5j.mongodb.net/" --apiVersion 1 --username elliotching --password BrZGKaBdWvtc2KEY
 // UI web access:
