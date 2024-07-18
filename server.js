29b1aa6521af8e63c79f29a535b93ad8111c2784
elliot <elliotching@hotmail.com>
2024-07-18 16:08:16 +0800

    save

--- server.js
+++ server.js
@@ -55,14 +55,14 @@ async function main() {
     }
 
     // Serve static files from the 'public' directory
-    // app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.static(path.join(__dirname, "public")));
 
     // Define a route to serve the custom error page
-    app.get("/app-error", (req, res) => {
-        res.sendFile(
-            path.join(__dirname, "public/app-error.html")
-        );
-    });
+    // app.get("/app-error", (req, res) => {
+    //     res.sendFile(
+    //         path.join(__dirname, "public/app-error.html")
+    //     );
+    // });
 
     // define a simple route
     app.get("/", (req, res) => {
